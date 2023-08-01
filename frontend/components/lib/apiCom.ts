c50364574e7b1c6551288172c8da30eaaaa3c26f
celdond <celdond@gmail.com>
2023-08-01 16:30:56 -0700

    login api call

--- frontend/components/lib/apiCom.ts
+++ frontend/components/lib/apiCom.ts
@@ -1,22 +1,24 @@
-import { serverUrl } from "../../serverConfig";
+import { serverUrl } from '../../serverConfig';
 
-export async function callServer(method: 'GET' | 'PUT' | 'POST' | 'DELETE',
-targetUrl: string,
-user?: string,
-body?: any): Promise<any> {
-    const options: RequestInit = { method };
-    options.headers = {};
-    if (user) {
-        const token = localStorage.getItem(`essentialMailToken/${user}`);
-        options.headers['authorization'] = `Bearer ${token}`;
-    }
-    if (body) {
-        options.body = JSON.stringify(body);
-        options.headers['content-type'] = 'application/json';
-    }
+export async function callServer(
+	targetUrl: string,
+	method: 'GET' | 'PUT' | 'POST' | 'DELETE',
+    body?: any,
+	user?: string,
+): Promise<Response> {
+	const options: RequestInit = { method };
+	options.headers = {};
+	if (user) {
+		const token = localStorage.getItem(`essentialMailToken/${user}`);
+		options.headers['authorization'] = `Bearer ${token}`;
+	}
+	if (body) {
+		options.body = JSON.stringify(body);
+		options.headers['content-type'] = 'application/json';
+	}
 
-    const url = serverUrl + targetUrl;
-    const request = await fetch(url, options);
+	const url = serverUrl + targetUrl;
+	const response = await fetch(url, options);
 
-    return request;
-};
+	return response;
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,20 +1,58 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import { Button, Container, Form } from 'react-bootstrap';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { callServer } from '../components/lib/apiCom';
 
 const Home: NextPage = () => {
-    const [user, setUser] = useState({email: '', password: ''});
-    
+	const router = useRouter();
+	const [user, setUser] = useState('');
+	const [passcode, setCode] = useState('');
+
+	async function submitLogin(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+
+		const loginInfo = {};
+		callServer('/login', 'POST', loginInfo)
+			.then((response) => {
+				if (!response.ok) {
+					throw response;
+				}
+				return response.json();
+			})
+			.then((json) => {
+				localStorage.setItem(
+					`essentialMailToken/${user}`,
+					JSON.stringify(json),
+				);
+				router.push(`/mail`);
+			})
+			.catch((err) => {
+				alert(`Error logging in, please try again.`);
+			});
+	}
+
 	return (
 		<Container>
-			<Form>
+			<Form onSubmit={submitLogin}>
 				<Form.Group>
 					<Form.Label>Email Address</Form.Label>
-					<Form.Control type="email" placeholder="Email" />
+					<Form.Control
+						type="email"
+						placeholder="Email"
+						value={user}
+						onChange={(e) => setUser(e.target.value)}
+					/>
 				</Form.Group>
 				<Form.Group className="mb-3" controlId="formBasicPassword">
 					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" placeholder="Password" />
+					<Form.Control
+						type="password"
+						placeholder="Password"
+						value={passcode}
+						onChange={(e) => setCode(e.target.value)}
+					/>
 				</Form.Group>
 			</Form>
 			<Button type="submit">Submit</Button>
