756d0e58e89439d0241463f4ab0f26e48808cc2b
celdond <celdond@gmail.com>
2023-08-03 18:53:08 -0700

    register page and password confirmation

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { Container, Form } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
+import { callServer } from '../components/lib/apiCom';
 
 const Register: NextPage = () => {
-    const router = useRouter();
-    const [email, setEmail] = useState('');
+	const router = useRouter();
+	const [email, setEmail] = useState('');
 	const [passcode, setCode] = useState('');
+	const [confirmcode, setConfirm] = useState('');
 
-    return (
+	async function submitRegister() {
+		if (passcode != confirmcode) {
+			alert('Passwords do not match.');
+		}
+		const registerInfo = { email: email, password: passcode };
+		callServer('/register', 'POST', registerInfo)
+			.then((response) => {
+				if (!response.ok) {
+					throw response;
+				}
+				router.push(`/`);
+				return;
+			})
+			.catch((err) => {
+				alert(`Error logging in, please try again.`);
+			});
+	}
+
+	return (
 		<Container>
-            <Form>
+			<Form>
 				<Form.Group>
 					<Form.Label>Email Address</Form.Label>
 					<Form.Control
@@ -30,9 +50,21 @@ const Register: NextPage = () => {
 						onChange={(e) => setCode(e.target.value)}
 					/>
 				</Form.Group>
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Password Confirmation</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Confirm Password"
+						value={confirmcode}
+						onChange={(e) => setConfirm(e.target.value)}
+					/>
+				</Form.Group>
 			</Form>
-        </Container>
-    );
+			<Button variant="primary" type="submit" onClick={submitRegister}>
+				Register
+			</Button>
+		</Container>
+	);
 };
 
 export default Register;
