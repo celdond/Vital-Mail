c90182b251a0791a38c36c7c96a536445a389943
celdond <celdond@gmail.com>
2023-08-04 23:04:38 -0700

    fixed logout Adjusted layout

---
+++ frontend/components/homePage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { Dropdown, Container, Navbar, Image } from 'react-bootstrap';
+
+const HomePage: NextPage = () => {
+	const router = useRouter();
+
+	const account = localStorage.getItem(`essentialMailToken`);
+	const user = JSON.parse(account);
+
+	const logout = () => {
+		localStorage.removeItem(`essentialMailToken`);
+		router.push('/');
+	};
+
+	return (
+		<Container>
+			<Navbar>
+				<Dropdown>
+					<Dropdown.Toggle variant="success" id="dropdown-basic">
+						<Image alt="A" roundedCircle={true} />
+					</Dropdown.Toggle>
+
+					<Dropdown.Menu>
+						<Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
+			</Navbar>
+			<Container>Hello!</Container>
+		</Container>
+	);
+};
+
+export default HomePage;
--- frontend/pages/mail/home.tsx
+++ frontend/pages/mail/home.tsx
@@ -1,35 +1,7 @@
-import React from 'react';
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { Dropdown, Container, Navbar, Image } from 'react-bootstrap';
+import HomePage from '../../components/homePage';
 
-const Home: NextPage = () => {
-	const router = useRouter();
-
-	const account = localStorage.getItem(`essentialMailToken`);
-	const user = JSON.parse(account);
-
-	const logout = () => {
-		localStorage.removeItem(`essentialMailToken`);
-		router.push('/');
-	};
-
-	return (
-		<Container>
-			<Navbar>
-				<Dropdown>
-					<Dropdown.Toggle variant="success" id="dropdown-basic">
-						<Image alt="A" roundedCircle={true} />
-					</Dropdown.Toggle>
-
-					<Dropdown.Menu>
-						<Dropdown.Item onclick={logout}>Logout</Dropdown.Item>
-					</Dropdown.Menu>
-				</Dropdown>
-			</Navbar>
-			<Container>Hello!</Container>
-		</Container>
-	);
+function Home () {
+    return (<HomePage />);
 };
 
 export default Home;
