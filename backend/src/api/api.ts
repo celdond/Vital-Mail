ebbee8c1be8f507089e1168f456d6ef7e0f92d45
celdond <celdond@gmail.com>
2023-08-07 21:49:11 -0700

    api set-up response

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -1,9 +1,10 @@
 import { Express } from 'express';
 import { login, register, check } from '../db/databaseHandler';
-import { getMailboxes } from './email';
+import { getMailboxes, getMail } from './email';
 
 export default function registerAPIRoutes(app: Express) {
     app.post("/login", login);
     app.post("/register", register);
     app.get("/mailbox", check, getMailboxes);
+    app.get("/mail", check, getMail);
 }
--- backend/src/api/email.ts
+++ backend/src/api/email.ts
@@ -15,3 +15,17 @@ export async function getMailboxes(req: CheckRequest, res: Response) {
     res.status(400).send("Bad Request.");
   }
 }
+
+export async function getMail(req: CheckRequest, res: Response) {
+  const usermail = req.usermail;
+  if (usermail) {
+    const mail = await accessMail(usermail, req.query.mailbox, req.query.from);
+    if (mail.length == 0) {
+      res.status(404).send();
+      return;
+    }
+    res.status(200).json(mail);
+  } else {
+    res.status(400).send("Bad Request.");
+  }
+}
