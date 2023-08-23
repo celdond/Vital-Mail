806e3c0bbba1644ec944a6c577267e6bdd0c0acf
celdond <celdond@gmail.com>
2023-08-23 00:49:46 -0700

    added index.html

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,19 +1,30 @@
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/login';
+import Home from './pages/home';
+import Register from './pages/register';
 
-const Authenticatation = ({children}) => {
-    if (localStorage.getItem(`essentialMailToken`)) {
-      return children;
-    }
-    return <Navigate to='/' replace />;
+const Authentication = ({ children }) => {
+	if (localStorage.getItem(`essentialMailToken`)) {
+		return children;
+	}
+	return <Navigate to="/" replace />;
 };
 
 export default function App() {
 	return (
 		<BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-            </Routes>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route
+					path="/home"
+					element={
+						<Authentication>
+							<Home />
+						</Authentication>
+					}
+				/>
+			</Routes>
 		</BrowserRouter>
 	);
 }
