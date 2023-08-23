ce8eed54277eb6de6bac5044733c36ec5238ec93
celdond <celdond@gmail.com>
2023-08-23 02:48:17 -0700

    sass working again

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-
-import './styles/globals.scss';
+import "./styles/globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
