928d0c6dce69f8fbe5204c4728784de1c3dd4ea6
celdond <celdond@gmail.com>
2023-09-06 19:19:04 -0700

    Compose Page set-up

---
+++ frontend/src/components/compose.tsx
@@ -0,0 +1,11 @@
+import { Container, Col } from 'react-bootstrap';
+
+export default function ComposePage() {
+	return (
+		<main className="backplate">
+			<Container className="mailpage">
+				<Col></Col>
+			</Container>
+		</main>
+	);
+}
---
+++ frontend/src/pages/compose.tsx
@@ -0,0 +1,6 @@
+import ComposePage from '../components/compose';
+
+export default function Compose() {
+
+	return (<ComposePage />);
+}
