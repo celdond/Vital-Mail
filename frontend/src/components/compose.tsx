3bccb355000e0cd2e714e929699b6d8f96a3432b
celdond <celdond@gmail.com>
2023-09-06 19:44:10 -0700

    sendMail function for compose

--- frontend/src/components/compose.tsx
+++ frontend/src/components/compose.tsx
@@ -1,9 +1,33 @@
-import { Container, Col, Form } from 'react-bootstrap';
+import { tokenType } from './lib/SharedContext';
+import { Container, Col, Form, Button } from 'react-bootstrap';
 import { BoxArrowLeft } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router-dom';
+import { callServer } from './lib/apiCom';
+
+const sendMail = (event: any, navigation: Function, user: tokenType) => {
+	const token = user ? user.token : null;
+	console.log(event);
+	callServer('/mail', 'POST', event, token)
+		.then((response) => {
+			if (!response.ok) {
+				console.log(response);
+				throw response;
+			}
+			return;
+		})
+		.then(() => {
+			navigation(-1);
+		})
+		.catch((err) => {
+			alert(`(${err}), please try again`);
+		});
+};
 
 export default function ComposePage() {
+	const account = localStorage.getItem(`essentialMailToken`);
+	const user = JSON.parse(account);
 	const navigation = useNavigate();
+
 	return (
 		<main className="backplate">
 			<Container className="mailpage">
@@ -12,10 +36,15 @@ export default function ComposePage() {
 						<BoxArrowLeft onClick={() => navigation(-1)} />
 					</div>
 					<div className="mailview">
-						<Form>
+						<Form onSubmit={(e) => sendMail(e, navigation, user)}>
+							<Button type="submit">Send</Button>
 							<Form.Group controlId="address">
 								<Form.Label>Address</Form.Label>
-								<Form.Control type="text" placeholder="Username" />
+								<Form.Control required type="text" />
+							</Form.Group>
+							<Form.Group controlId="subject">
+								<Form.Label>Subject</Form.Label>
+								<Form.Control required type="text" />
 							</Form.Group>
 							<Form.Group controlId="content">
 								<Form.Label>Message</Form.Label>
