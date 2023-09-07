a146c1b684ba726b3c4e4f72bf0d909fd05bba80
celdond <celdond@gmail.com>
2023-09-06 19:29:32 -0700

    Basic Compose page structure

--- frontend/src/components/compose.tsx
+++ frontend/src/components/compose.tsx
@@ -1,10 +1,29 @@
-import { Container, Col } from 'react-bootstrap';
+import { Container, Col, Form } from 'react-bootstrap';
+import { BoxArrowLeft } from 'react-bootstrap-icons';
+import { useNavigate } from 'react-router-dom';
 
 export default function ComposePage() {
+	const navigation = useNavigate();
 	return (
 		<main className="backplate">
 			<Container className="mailpage">
-				<Col></Col>
+				<Col>
+					<div className="simpleBar">
+						<BoxArrowLeft onClick={() => navigation(-1)} />
+					</div>
+					<div className="mailview">
+						<Form>
+							<Form.Group controlId="address">
+								<Form.Label>Address</Form.Label>
+								<Form.Control type="text" placeholder="Username" />
+							</Form.Group>
+							<Form.Group controlId="content">
+								<Form.Label>Message</Form.Label>
+								<Form.Control as="textarea" rows={5} />
+							</Form.Group>
+						</Form>
+					</div>
+				</Col>
 			</Container>
 		</main>
 	);
