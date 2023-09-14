5ebeaf8efa350a65fa5ba21be60607faf0042867
celdond <celdond@gmail.com>
2023-09-13 19:20:46 -0700

    core page layout

--- frontend/src/components/accountPage.tsx
+++ frontend/src/components/accountPage.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { Container, Col, Form } from 'react-bootstrap';
+import { BoxArrowLeft } from 'react-bootstrap-icons';
 
 export default function AccountPage() {
 	const account = localStorage.getItem(`essentialMailToken`);
@@ -18,5 +20,47 @@ export default function AccountPage() {
 
 	const user = JSON.parse(account);
 	const navigation = useNavigate();
-	return <div></div>;
+	return (
+		<main className="backplate">
+			<Container className="mailpage">
+				<Col className="full">
+					<div className="simpleBar">
+						<BoxArrowLeft onClick={() => navigation(-1)} />
+						<BoxArrowLeft />
+					</div>
+					<div className="mailview">
+						<Form className="full">
+							<Form.Group controlId="name">
+								<Form.Label>Name</Form.Label>
+								<Form.Control
+									name="name"
+									onChange={handleChange}
+									required
+									type="text"
+								/>
+							</Form.Group>
+							<Form.Group controlId="email">
+								<Form.Label>Email</Form.Label>
+								<Form.Control
+									name="email"
+									onChange={handleChange}
+									required
+									type="text"
+								/>
+							</Form.Group>
+							<Form.Group controlId="password">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									name="password"
+									onChange={handleChange}
+									required
+									type="password"
+								/>
+							</Form.Group>
+						</Form>
+					</div>
+				</Col>
+			</Container>
+		</main>
+	);
 }
