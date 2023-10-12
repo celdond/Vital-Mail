15574a25ceb281b04e272c710722843d4fac9f7d
celdond <celdond@gmail.com>
2023-10-12 00:59:08 -0700

    delete box tests

--- backend/src/__tests__/customBox.test.ts
+++ backend/src/__tests__/customBox.test.ts
@@ -41,8 +41,8 @@ test("Successful Login", async () => {
     });
 });
 
-// Get Mailboxes
-test("GET Missing Mailbox", async () => {
+// Create New Custom Mailbox
+test("Custom Mailbox", async () => {
   await request
     .post("/mailbox")
     .set({ authorization: "Bearer " + token })
@@ -50,8 +50,8 @@ test("GET Missing Mailbox", async () => {
     .expect(201);
 });
 
-// Get Mailboxes
-test("GET Missing Mailbox", async () => {
+// Check to Make Sure New Mailbox was Added
+test("Get Mailboxes", async () => {
   await request
     .get("/mailbox")
     .set({ authorization: "Bearer " + token })
@@ -61,5 +61,36 @@ test("GET Missing Mailbox", async () => {
       expect(data.body).toBeDefined();
       expect(data.body.length).toBeDefined();
       expect(data.body.length).toStrictEqual(4);
+    });
+});
+
+test("Delete Forbidden Mailbox", async () => {
+  await request
+    .delete("/mailbox")
+    .set({ authorization: "Bearer " + token })
+    .send({ boxName: "Inbox" })
+    .expect(403);
+});
+
+// Delete Custom Mailbox
+test("Delete Custom Mailbox", async () => {
+  await request
+    .delete("/mailbox")
+    .set({ authorization: "Bearer " + token })
+    .send({ boxName: "Custom" })
+    .expect(200);
+});
+
+// Check to Make Sure Mailbox was Deleted
+test("Get Mailboxes", async () => {
+  await request
+    .get("/mailbox")
+    .set({ authorization: "Bearer " + token })
+    .expect(200)
+    .then((data) => {
+      expect(data).toBeDefined();
+      expect(data.body).toBeDefined();
+      expect(data.body.length).toBeDefined();
+      expect(data.body.length).toStrictEqual(3);
     });
 });
