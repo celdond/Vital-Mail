99f088755aaa0677bb517c2d44fe5283e5cc9323
celdond <celdond@gmail.com>
2023-11-21 21:04:59 -0800

    formatting

--- backend/src/__tests__/validation.test.ts
+++ backend/src/__tests__/validation.test.ts
@@ -1,31 +1,31 @@
-import {validatePassword, validateName} from "../db/accountHandler";
+import { validatePassword, validateName } from "../db/accountHandler";
 
 test("Validation Name - Success", async () => {
-    const name = await validateName("Hello66");
-    expect(name).toBe(true);
+  const name = await validateName("Hello66");
+  expect(name).toBe(true);
 });
 
 test("Validation Name - Fail Special Characters", async () => {
-    const name = await validateName("Hello66!");
-    expect(name).toBe(false);
+  const name = await validateName("Hello66!");
+  expect(name).toBe(false);
 });
 
 test("Validation Password - Success", async () => {
-    const password = await validatePassword("Hello66");
-    expect(password).toBe(true);
+  const password = await validatePassword("Hello66");
+  expect(password).toBe(true);
 });
 
 test("Validation Password - Fail @", async () => {
-    const password = await validatePassword("H@@66");
-    expect(password).toBe(false);
+  const password = await validatePassword("H@@66");
+  expect(password).toBe(false);
 });
 
 test("Validation Password - Success Special Characters", async () => {
-    const password = await validatePassword("!!Hell!?o66");
-    expect(password).toBe(true);
+  const password = await validatePassword("!!Hell!?o66");
+  expect(password).toBe(true);
 });
 
 test("Validation Password - Only Special Characters", async () => {
-    const password = await validatePassword("!#%$");
-    expect(password).toBe(true);
+  const password = await validatePassword("!#%$");
+  expect(password).toBe(true);
 });
